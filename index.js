1234a9933d0a8f8560a1f0455d0b95944157f0e5
BrynBlumenstein <90479522+BrynBlumenstein@users.noreply.github.com>
2025-08-10 14:30:37 -0400

    Add .env and refactor to use MongoDB with Mongoose

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+require('dotenv').config();
 const express = require('express');
 // const cors = require('cors');
+const mongoose = require('mongoose');
 const morgan = require('morgan');
+const Note = require('./models/note');
+
 const app = express();
 
 app.use(express.json());
@@ -15,55 +19,27 @@ app.use(
 
 app.use(express.static('dist'));
 
-let notes = [
-	{
-		id: '1',
-		content: 'HTML is easy',
-		important: true
-	},
-	{
-		id: '2',
-		content: 'Browser can execute only JavaScript',
-		important: false
-	},
-	{
-		id: '3',
-		content: 'GET and POST are the most important methods of HTTP protocol',
-		important: true
-	}
-];
-
 app.get('/', (request, response) => {
 	response.send('<h1>Hello World!</h1>');
 });
 
 app.get('/api/notes', (request, response) => {
-	response.json(notes);
+	Note.find({}).then((notes) => {
+		response.json(notes);
+	});
 });
 
 app.get('/api/notes/:id', (request, response) => {
-	const id = request.params.id;
-	const note = notes.find((note) => note.id === id);
-
-	if (note) {
+	Note.findById(request.params.id).then((note) => {
 		response.json(note);
-	} else {
-		response.status(404).end();
-	}
+	});
 });
 
 app.delete('/api/notes/:id', (request, response) => {
-	const id = request.params.id;
-	notes = notes.filter((note) => note.id !== id);
-
-	response.status(204).end();
+	Note.findByIdAndDelete(request.params.id).then(() => {
+		response.status(204).end();
+	});
 });
-
-const generateId = () => {
-	const maxId =
-		notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
-	return String(maxId + 1);
-};
 
 app.post('/api/notes', (request, response) => {
 	const body = request.body;
@@ -72,37 +48,33 @@ app.post('/api/notes', (request, response) => {
 		return response.status(400).json({ error: 'content missing' });
 	}
 
-	const note = {
-        id: generateId(),
+	const note = new Note({
 		content: body.content,
 		important: body.important || false
-	};
+	});
 
-	notes = notes.concat(note);
-
-	response.json(note);
+	note.save().then((savedNote) => {
+		response.json(savedNote);
+	});
 });
 
 app.put('/api/notes/:id', (request, response) => {
 	const body = request.body;
 
-    if (!body.content) {
+	if (!body.content) {
 		return response.status(400).json({ error: 'content missing' });
 	}
 
-    if (!body.id) {
-		return response.status(400).json({ error: 'id missing' });
-	}
-
-    const note = {
-        id: body.id,
+	const noteUpdates = {
 		content: body.content,
 		important: body.important || false
 	};
 
-    notes = notes.map((n) => n.id !== body.id ? n : note);
-
-    response.json(note);
+	Note.findByIdAndUpdate(request.params.id, noteUpdates, { new: true }).then(
+		(updatedNote) => {
+			response.json(updatedNote);
+		}
+	);
 });
 
 const unknownEndpoint = (request, response) => {
@@ -111,7 +83,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
 });
