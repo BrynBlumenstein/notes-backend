ef3c4a54514d34e0f08abb75052273ed74a65ee4
BrynBlumenstein <90479522+BrynBlumenstein@users.noreply.github.com>
2025-08-10 07:03:49 -0400

    Remove unused CORS dependency and related code

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const cors = require('cors');
+// const cors = require('cors');
 const morgan = require('morgan');
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 morgan.token('data', (request) => JSON.stringify(request.body));
 app.use(
