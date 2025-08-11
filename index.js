958d5921ef798361e9c4c640778d9f941b53434f
BrynBlumenstein <90479522+BrynBlumenstein@users.noreply.github.com>
2025-08-11 07:53:54 -0400

    Add error handling to routes and refactor update note route

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const Note = require('./models/note');
 
 const app = express();
 
+// pre-routes middleware
+app.use(express.static('dist'));
 app.use(express.json());
 // app.use(cors());
 
@@ -17,8 +19,7 @@ app.use(
 	)
 );
 
-app.use(express.static('dist'));
-
+// routes
 app.get('/', (request, response) => {
 	response.send('<h1>Hello World!</h1>');
 });
@@ -29,16 +30,24 @@ app.get('/api/notes', (request, response) => {
 	});
 });
 
-app.get('/api/notes/:id', (request, response) => {
-	Note.findById(request.params.id).then((note) => {
-		response.json(note);
-	});
+app.get('/api/notes/:id', (request, response, next) => {
+	Note.findById(request.params.id)
+		.then((note) => {
+			if (note) {
+				response.json(note);
+			} else {
+				response.status(404).end();
+			}
+		})
+		.catch((error) => next(error));
 });
 
-app.delete('/api/notes/:id', (request, response) => {
-	Note.findByIdAndDelete(request.params.id).then(() => {
-		response.status(204).end();
-	});
+app.delete('/api/notes/:id', (request, response, next) => {
+	Note.findByIdAndDelete(request.params.id)
+		.then((result) => {
+			response.status(204).end();
+		})
+		.catch((error) => next(error));
 });
 
 app.post('/api/notes', (request, response) => {
@@ -58,31 +67,45 @@ app.post('/api/notes', (request, response) => {
 	});
 });
 
-app.put('/api/notes/:id', (request, response) => {
-	const body = request.body;
+app.put('/api/notes/:id', (request, response, next) => {
+	const { content, important } = request.body;
 
-	if (!body.content) {
-		return response.status(400).json({ error: 'content missing' });
-	}
+	Note.findById(request.params.id)
+		.then((note) => {
+			if (!note) {
+				return response.status(404).end();
+			}
 
-	const noteUpdates = {
-		content: body.content,
-		important: body.important || false
-	};
+			note.content = content;
+			note.important = important;
 
-	Note.findByIdAndUpdate(request.params.id, noteUpdates, { new: true }).then(
-		(updatedNote) => {
-			response.json(updatedNote);
-		}
-	);
+			return note.save().then((updatedNote) => {
+				response.json(updatedNote);
+			});
+		})
+		.catch((error) => next(error));
 });
 
+// post-routes middleware
 const unknownEndpoint = (request, response) => {
 	response.status(404).send({ error: 'unknown endpoint' });
 };
 
 app.use(unknownEndpoint);
 
+const errorHandler = (error, request, response, next) => {
+	console.error(error.message);
+
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'malformatted id' });
+	}
+
+	next(error);
+};
+
+app.use(errorHandler);
+
+// start server
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
