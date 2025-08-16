ab70318fe608363bd4b6856c430aab6a622d9aa4
BrynBlumenstein <90479522+BrynBlumenstein@users.noreply.github.com>
2025-08-16 08:04:50 -0400

    Remove stylistic dependency and update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,5 @@
 import globals from 'globals';
 import js from '@eslint/js';
-import stylisticJs from '@stylistic/eslint-plugin-js';
 
 export default [
 	js.configs.recommended,
@@ -11,21 +10,19 @@ export default [
 			globals: { ...globals.node },
 			ecmaVersion: 'latest'
 		},
-		plugins: {
-			'@stylistic/js': stylisticJs
-		},
 		rules: {
-			'@stylistic/js/linebreak-style': ['error', 'unix'],
-			'@stylistic/js/quotes': ['error', 'single'],
-			'@stylistic/js/semi': ['error', 'always'],
+			'linebreak-style': ['error', 'unix'],
+			quotes: ['error', 'single'],
+			semi: ['error', 'always'],
 			eqeqeq: 'error',
 			'no-trailing-spaces': 'error',
 			'object-curly-spacing': ['error', 'always'],
 			'arrow-spacing': ['error', { before: true, after: true }],
-			'no-console': 'off'
+			'no-console': 'off',
+			'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }]
 		}
 	},
 	{
-		ignores: ['dist/**']
+		ignorePatterns: ['dist/**']
 	}
 ];
